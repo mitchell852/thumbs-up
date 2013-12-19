3a75d199e3095256406b0ac2c5844301fcc9b1ab
Jeremy Mitchell <jeremy@flexdevelopers.com>
2013-12-19 14:40:55 -0700

    initial tests for thumb model

--- spec/models/thumb_spec.rb
+++ spec/models/thumb_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Thumb do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @thumb = Thumb.new(label: 'The Water Jet Pack') }
+  subject { @thumb }
+
+  it { should respond_to(:active) }
+  it { should respond_to(:label) }
+  it { should respond_to(:image_path) }
+
+  its(:active) { should be_false }
+  its(:label) { should == 'The Water Jet Pack' }
+  its(:image_path) { should be_blank }
+
+  it { should be_valid }
 end
