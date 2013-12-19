cac7503434d31bf7dccbb291bdfbb392cc6d113f
Jeremy Mitchell <jeremy@flexdevelopers.com>
2013-12-19 12:03:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThumbsUp::Application.config.secret_key_base = 'f882497bde0c28495b7bda70a449892e2da6c1c2d27e58ceef416946f8e99b0ee569964cb33f635df33846930df70e2f6f9333a0e3d45656bc8267472dcce4c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThumbsUp::Application.config.session_store :cookie_store, key: '_thumbs-up_session'
