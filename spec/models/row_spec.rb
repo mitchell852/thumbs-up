1372e5d030c798b92afd7f014a6c388457dc175e
Jeremy Mitchell <jeremy@flexdevelopers.com>
2013-12-19 20:28:34 -0700

    testing the row to thumb relationship

--- spec/models/row_spec.rb
+++ spec/models/row_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe Row do
+  before { @row = Row.new }
+
   it { should respond_to(:thumbs) }
 
-  it 'includes thumbs' do
-    thumb1 = Thumb.new(label: 'Doctor Stranglove')
-    thumb2 = Thumb.new(label: 'Doctor Phil')
-    row = Row.new(thumbs: [thumb1, thumb2])
-    row.thumbs.should include(thumb1)
-    row.thumbs.should include(thumb2)
+  it 'changes the number of rows by 1 when saved' do
+    expect { @row.save }.to change { Row.count }.by(1)
   end
+
+  it 'includes thumbs' do
+    thumb1 = Thumb.create(label: 'Doctor Stranglove', row: @row)
+    thumb2 = Thumb.create(label: 'Doctor Phil', row: @row)
+
+    @row.thumbs.should include(thumb1)
+    @row.thumbs.should include(thumb2)
+    @row.should have(2).thumbs
+  end
+
 end
