a9ff87576d026f5e1bb791629a7333c1b4634a45
Jeremy Mitchell <jeremy@flexdevelopers.com>
2013-12-19 21:42:50 -0700

    add nested routes for row --> thumb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ThumbsUp::Application.routes.draw do
-  resources :thumbs
+  resources :rows do
+    resources :thumbs
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
