18890fffe85bd62bf1b8c70a14fed26be3af3a93
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-01-30 20:19:59 -0700

    adds hardcoded html, css and js for thumb details

---
+++ app/assets/javascripts/rows.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.rs-carousel').carousel({
+      speed: 700,
+      itemsPerPage: 1,
+      itemsPerTransition: 1,
+      itemWidth: 870,
+      containerHeight: 410,
+      calculateWidth: true,
+      moveNextActionRight: true,
+      swipeEnabled: true
+    });
+});
+
